c655d247250747591d58f463b88443820d93482e
evandeters <evanjd711@gmail.com>
2024-01-12 20:32:10 -0800

    Lazy nvim

--- lua/evan/remap.lua
+++ lua/evan/remap.lua
@@ -21,7 +21,3 @@ vim.keymap.set("v", "<leader>d", "\"_d")
 
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-
-vim.keymap.set("n", "<leader><leader>", function()
-    vim.cmd("so")
-end)
