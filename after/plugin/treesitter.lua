3b6ae8d70c6986e3f38852c90f87fe91a3caa1b9
evandeters <evanjd711@gmail.com>
2024-05-15 18:39:11 -0700

    added oil

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,8 @@
-require'nvim-treesitter.configs'.setup {
+require 'nvim-treesitter.install'.compilers = { "zig" }
+require 'nvim-treesitter.install'.prefer_git = true
+require 'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "html", "css", "json", "yaml", "python", "bash", "toml", "comment", "go", "lua", "dockerfile", "java", "query", "vue" },
+  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "html", "css", "json", "yaml", "python", "bash", "toml", "comment", "go", "lua", "dockerfile", "java", "query", "vue", "javascript", "pkl" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
--- lua/evan/init.lua
+++ lua/evan/init.lua
@@ -2,14 +2,14 @@ require("evan.remap")
 require("evan.set")
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -22,43 +22,50 @@ return require('lazy').setup({
     'mbbill/undotree',
     'tpope/vim-fugitive',
     'nvim-treesitter/nvim-treesitter',
+    'apple/pkl-neovim',
     'theprimeagen/vim-be-good',
 
-  {
-          'nvim-telescope/telescope.nvim', tag = '0.1.5',
-          -- or                            , branch = '0.1.x',
-          dependencies = { {'nvim-lua/plenary.nvim'} }
-  },
+    {
+        'stevearc/oil.nvim',
+        opts = {},
+    },
 
-  {
-          'rose-pine/neovim',
-          as = 'rose-pine',
-          config = function()
-                  vim.cmd('colorscheme rose-pine')
-          end
-  },
 
-  {
-          "ThePrimeagen/harpoon",
-          branch = "harpoon2",
-          dependencies = { {"nvim-lua/plenary.nvim"} }
-  },
+    {
+        'nvim-telescope/telescope.nvim', tag = '0.1.5',
+        -- or                            , branch = '0.1.x',
+        dependencies = { {'nvim-lua/plenary.nvim'} }
+    },
 
-  {
-          'VonHeikemen/lsp-zero.nvim',
-          branch = 'v3.x',
-          dependencies = {
-                  --- Uncomment these if you want to manage LSP servers from neovim
-                  {'williamboman/mason.nvim'},
-                  {'williamboman/mason-lspconfig.nvim'},
+    {
+        'rose-pine/neovim',
+        as = 'rose-pine',
+        config = function()
+            vim.cmd('colorscheme rose-pine')
+        end
+    },
 
-                  -- LSP Support
-                  {'neovim/nvim-lspconfig'},
-                  -- Autocompletion
-                  {'hrsh7th/nvim-cmp'},
-                  {'hrsh7th/cmp-nvim-lsp'},
-                  {'L3MON4D3/LuaSnip'},
-          }
-  },
+    {
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+        dependencies = { {"nvim-lua/plenary.nvim"} }
+    },
+
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x',
+        dependencies = {
+            --- Uncomment these if you want to manage LSP servers from neovim
+            {'williamboman/mason.nvim'},
+            {'williamboman/mason-lspconfig.nvim'},
+
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},
+            {'hrsh7th/cmp-nvim-lsp'},
+            {'L3MON4D3/LuaSnip'},
+        }
+    },
 
 })
